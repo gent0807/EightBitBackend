0ef376eb5e9495df24b3030cb174af9df12c3eba
gent0807 <theloopholesnk@gmail.com>
2023-05-23 05:19:16 +0900

    이메일 인증 구현(암호화 제외) 머지 완료

--- src/main/java/com/eightbit/biz/user/inter/MailSendService.java
+++ src/main/java/com/eightbit/biz/user/inter/MailSendService.java
@@ -6,19 +6,12 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import javax.mail.MessagingException;
 
 public interface MailSendService {
-<<<<<<< HEAD
-    public void sendAuthNumToEmail(String setFrom, String toMail, String title,String content);
-    public void makeRandomNumber();
-    public String joinEmail(String email);
 
-
-=======
-    //public String sendAuthKey(String email);
     public void makeRandomNumber();
 
 
 
     public String mailSend(String email);
 
->>>>>>> test2
+
 }
